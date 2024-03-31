d5fff7531ef8729786f9a266497cb6b36f12cbf3
Vishal <Vishalarya2468@gmail.com>
2024-03-31 18:32:57 +0530

    feat:sign-up page added

--- src/components/MaxWidthWrapper.tsx
+++ src/components/MaxWidthWrapper.tsx
@@ -8,11 +8,11 @@ const MaxWidthWrapper = ({
   className?: string;
   children: ReactNode;
 }) => {
-  return (
+  return(
     <div
       className={cn(
         "mx-auto w-full max-w-screen-xl px-2.5 md:px-20",
-        className
+        className 
       )}
     >
       {children}
--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,56 +1,54 @@
-"use client"
+"use client";
 import { PRODUCT_CATEGORIES } from "@/config";
-import { useState,useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 import NavItem from "./NavItem";
 import { useOnClickOutside } from "@/hooks/use-on-click-outside";
 
 const NavItems = () => {
-    const [activeIndex, setActiveIndex] = useState< null | number>(null);
+  const [activeIndex, setActiveIndex] = useState<null | number>(null);
 
-    const isAnyOpen = activeIndex !==null;
+  const isAnyOpen = activeIndex !== null;
 
-    useEffect(() => {
-      const handler =(e:KeyboardEvent)=>{
-        if(e.key === "Escape"){
-            setActiveIndex(null);
-        }
-      } 
-    
-      document.addEventListener("keydown",handler)
-      return () => {
-        document.removeEventListener("keydown",handler)
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        setActiveIndex(null);
       }
-    }, [])
+    };
 
-    const navRef = useRef<HTMLDivElement | null>(null);
-    useOnClickOutside(navRef, () => setActiveIndex(null))
-    
+    document.addEventListener("keydown", handler);
+    return () => {
+      document.removeEventListener("keydown", handler);
+    };
+  }, []);
+
+  const navRef = useRef<HTMLDivElement | null>(null);
+  useOnClickOutside(navRef, () => setActiveIndex(null));
+
   return (
     <div className="flex gap-4 h-full" ref={navRef}>
-        {
-            PRODUCT_CATEGORIES.map((category,i)=>{
-                const handleOpen = () =>{
-                    if(activeIndex===i){
-                        setActiveIndex(null);
-                    }else{
-                        setActiveIndex(i);
-                    }
-                }
+      {PRODUCT_CATEGORIES.map((category, i) => {
+        const handleOpen = () => {
+          if (activeIndex === i) {
+            setActiveIndex(null);
+          } else {
+            setActiveIndex(i);
+          }
+        };
 
-                const isOpen = i===activeIndex;
-                return (
-                    <NavItem 
-                    key={category.value}
-                    category={category}
-                     handleOpen={handleOpen} 
-                     isOpen={isOpen}
-                     isAnyOpen={isAnyOpen}
-                     />
-                )
-            })
-        }
+        const isOpen = i === activeIndex;
+        return (
+          <NavItem
+            key={category.value}
+            category={category}
+            handleOpen={handleOpen}
+            isOpen={isOpen}
+            isAnyOpen={isAnyOpen}
+          />
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
 export default NavItems;
